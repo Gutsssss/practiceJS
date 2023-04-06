6d6e21c7f1d7e190221cec8bbcc6bb60ceb54945
Gutsss <kacill1@mail.ru>
2023-04-06 11:21:53 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script setup>
+const obj1 = {
+value: 1,
+name:"Misha",
+surname:"Letov",
+age:21
 }
+const clone = {}
+
+for (let key in obj1) {
+clone[key] = obj1[key]
+
+}
+console.log(obj1)
+console.log(clone)
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
